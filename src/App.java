67fed865f5e6fb4f2bd7fa32e0c5b3741f7d4a52
sdoste <sergiodoste@gmail.com>
2024-04-25 21:28:35 +0200

    first commit

--- src/App.java
+++ src/App.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // // 1- SALUDAR!
-        // // Pides saludo y nombre y te responde ‘Hola Andrés’.
-        // // 1.1- Llamas a la función, sin parámetros, y sin return, y te hace el programa (resultado ejemplo: ‘Hola Andrés’).
-        // saludar1();
-        // // 1.2 -- Llamas a la función, con parámetros (saludo y nombre), y sin return, y muestra el resultado DENTRO de la función (resultado ejemplo: ‘Hola Andrés’).
-        // Scanner teclado = new Scanner(System.in); 
-        // System.out.println("Dime tu saludo preferido.");
-        // String saludo2 = teclado.nextLine();
-        // System.out.println("Dime tu nombre.");
-        // String nombre2 = teclado.nextLine();
-        // saludar2(saludo2, nombre2);
-        // // 1.3- Llamas a la función, con parámetros (saludo y nombre), y con return, y muestra el resultado FUERA de ese método, y dentro del main() (resultado ejemplo: ‘Hola Andrés’).
-        // System.out.println("Dime tu saludo preferido.");
-        // String saludo3 = teclado.nextLine();
-        // System.out.println("Dime tu nombre.");
-        // String nombre3 = teclado.nextLine();
-        // System.out.println(saludar3(saludo3, nombre3));
+        // 1- SALUDAR!
+        // Pides saludo y nombre y te responde ‘Hola Andrés’.
+        // 1.1- Llamas a la función, sin parámetros, y sin return, y te hace el programa (resultado ejemplo: ‘Hola Andrés’).
+        saludar1();
+        // 1.2 -- Llamas a la función, con parámetros (saludo y nombre), y sin return, y muestra el resultado DENTRO de la función (resultado ejemplo: ‘Hola Andrés’).
+        Scanner teclado = new Scanner(System.in); 
+        System.out.println("Dime tu saludo preferido.");
+        String saludo2 = teclado.nextLine();
+        System.out.println("Dime tu nombre.");
+        String nombre2 = teclado.nextLine();
+        saludar2(saludo2, nombre2);
+        // 1.3- Llamas a la función, con parámetros (saludo y nombre), y con return, y muestra el resultado FUERA de ese método, y dentro del main() (resultado ejemplo: ‘Hola Andrés’).
+        System.out.println("Dime tu saludo preferido.");
+        String saludo3 = teclado.nextLine();
+        System.out.println("Dime tu nombre.");
+        String nombre3 = teclado.nextLine();
+        System.out.println(saludar3(saludo3, nombre3));
 
         // 2- CALCULAR
         // Con switch, preguntar la operación, y con switch envíalo a una función diferente
@@ -78,38 +78,27 @@ public class App {
         // 3- CACATÚA CACAHUETES!
         cacatuaCacahuetes();
 
-        // Tienes una cacatúa que todo el rato te pide comer cacahuetes, diciendo “cacatúa cacahuetes!”.
-
-        // El programa te pregunta qué comida le das.
-
-        // Mientras NO sean cacahuetes, sigue diciendo “cacatúa cacahuetes!”
-
-
-        // Pero si le das cacahuetes, llama al método ‘dormirse’ y se calla, y en vez del mensaje anterior,
-        // vemos ‘zzZzzzz…’ (aquí acaba el programa)
-
-        // *EXTRA: realizar no solo el método 'dormirse()', sino también el de 'pideComida()'
         teclado.close();
     }
 
 
-    // public static void saludar1(){
-    //     // 1.1- Llamas a la función, sin parámetros, y sin return, y te hace el programa (resultado ejemplo: ‘Hola Andrés’).
-    //     Scanner teclado = new Scanner(System.in); 
-    //     System.out.println("Dime tu saludo preferido.");
-    //     String saludo = teclado.nextLine();
-    //     System.out.println("Dime tu nombre.");
-    //     String nombre = teclado.nextLine();
-    //     System.out.println("¡" + saludo + " " + nombre + "!");
-    // }
-    // public static void saludar2(String saludo, String nombre){
-    //     // 1.2 -- Llamas a la función, con parámetros (saludo y nombre), y sin return, y muestra el resultado DENTRO de la función (resultado ejemplo: ‘Hola Andrés’).
-    //     System.out.println("¡" + saludo + " " + nombre + "!");
-    // }
-    // public static String saludar3(String saludo, String nombre){
-    //     // 1.3- Llamas a la función, con parámetros (saludo y nombre), y con return, y muestra el resultado FUERA de ese método, y dentro del main() (resultado ejemplo: ‘Hola Andrés’).
-    //     return ("¡" + saludo + " " + nombre + "!");
-    // }
+    public static void saludar1(){
+        // 1.1- Llamas a la función, sin parámetros, y sin return, y te hace el programa (resultado ejemplo: ‘Hola Andrés’).
+        Scanner teclado = new Scanner(System.in); 
+        System.out.println("Dime tu saludo preferido.");
+        String saludo = teclado.nextLine();
+        System.out.println("Dime tu nombre.");
+        String nombre = teclado.nextLine();
+        System.out.println("¡" + saludo + " " + nombre + "!");
+    }
+    public static void saludar2(String saludo, String nombre){
+        // 1.2 -- Llamas a la función, con parámetros (saludo y nombre), y sin return, y muestra el resultado DENTRO de la función (resultado ejemplo: ‘Hola Andrés’).
+        System.out.println("¡" + saludo + " " + nombre + "!");
+    }
+    public static String saludar3(String saludo, String nombre){
+        // 1.3- Llamas a la función, con parámetros (saludo y nombre), y con return, y muestra el resultado FUERA de ese método, y dentro del main() (resultado ejemplo: ‘Hola Andrés’).
+        return ("¡" + saludo + " " + nombre + "!");
+    }
     public static Integer sumar(Integer numero1, Integer numero2){
         return (numero1 + numero2);
     }
@@ -142,6 +131,10 @@ public class App {
         }
     }
 
+    // Tienes una cacatúa que todo el rato te pide comer cacahuetes, diciendo “cacatúa cacahuetes!”.
+
+    // El programa te pregunta qué comida le das Mientras NO sean cacahuetes, sigue diciendo “cacatúa cacahuetes!”
+    // Pero si le das cacahuetes, llama al método ‘dormirse’ y se calla, y vemos ‘zzZzzzz…’ métodos 'dormirse()' y 'pideComida()'
     public static void cacatuaCacahuetes(){
         Scanner teclado = new Scanner(System.in);
         String comida = "";
